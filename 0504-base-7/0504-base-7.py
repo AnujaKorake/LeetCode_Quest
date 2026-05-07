d2b6e3c3ddb4da2920ac2394c2a4471d0f1a68cc
Anuja Mahadev Korake <147719075+AnujaKorake@users.noreply.github.com>
2026-05-07 22:48:03 +0530

    Time: 0 ms (100%), Space: 19.3 MB (33.01%) - LeetHub

---
+++ 0504-base-7/0504-base-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        
+        if num == 0:
+            return "0"
+
+        sign = ""
+        
+        if num < 0:
+            sign = "-"
+            num = abs(num)
+
+        ans = ""
+
+        while num != 0:
+            ans = str(num % 7) + ans
+            num //= 7
+
+        return sign + ans
